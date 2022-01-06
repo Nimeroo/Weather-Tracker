affc7381aea838c00744482e0c847efdbee75fb3
Nimeroo <girou.joe@gmail.com>
2022-01-06 04:52:43 -0500

    function call name changed

--- client/src/Services/api-config.js
+++ client/src/Services/api-config.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const domain = "http://api.weatherapi.com/v1";
 const apiKey = process.env.REACT_APP_API_KEY;
 
-export const weatherForecast = async (location) => {
+export const getWeatherForecast = async (location) => {
   const weatherResp = await axios.get(
     `${domain}/forecast.json?key=${apiKey}q=${location}`
   );
