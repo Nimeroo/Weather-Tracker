6dbf37b32da1c6b3e8e26df21d480c1b1f114ff0
Nimeroo <girou.joe@gmail.com>
2022-01-06 06:54:14 -0500

    url reconfigured to return 3 days of weather forecast instead of only current

--- client/src/Services/api-config.js
+++ client/src/Services/api-config.js
@@ -5,7 +5,7 @@ const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
 
 export const getWeatherForecast = async (location) => {
   const weatherResp = await axios.get(
-    `${domain}/forecast.json?key=${apiKey}&q=${location}`
+    `${domain}/forecast.json?key=${apiKey}&q=${location}&days=3`
   );
   return weatherResp;
 };
