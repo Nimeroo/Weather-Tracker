34926d964a03e421ac15b976dff6e2ab0153c93b
Nimeroo <girou.joe@gmail.com>
2022-01-06 05:58:09 -0500

    comments added

--- client/src/Util/locationUrlParse.js
+++ client/src/Util/locationUrlParse.js
@@ -1,3 +1,5 @@
+//Inserts "+" between seperate words in locations to pass as a search parameter in API url 
+
 export const locationURLParse = (location) => {
     let splitLocation = location.split(" ");
     let rejoinedLocation = splitLocation.join("+")
