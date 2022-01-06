6760c5eab855223f8c6d41629c72d87395b4b093
Nimeroo <girou.joe@gmail.com>
2022-01-06 04:50:14 -0500

    useStates for selected weather and overall forecast added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  const [selectedWeather, setSelectedWeather] = useState({});
+  const [forecast, setForecast] = useState([]);
+  return <div className="App"></div>;
 }
 
 export default App;
