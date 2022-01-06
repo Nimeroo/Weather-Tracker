2fc5f629a841c1ff8205460c7b9bb49eaa62c857
Nimeroo <girou.joe@gmail.com>
2022-01-06 05:30:09 -0500

    location useState added and forecast request added to useEffect

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
 import { useEffect, useState } from "react";
+import { getWeatherForecast } from "./Services/api-config";
 
 function App() {
   const [selectedWeather, setSelectedWeather] = useState({});
   const [forecast, setForecast] = useState([]);
-  return <div className="App"></div>;
+  const [location, setLocation] = useState("");
+
+  useEffect(async () => {
+    let fetchForecast = await getWeatherForecast(location);
+    setForecast(fetchForecast);
+  }, [location]);
+
+  return (
+    <div className="App">
+    </div>
+  );
 }
 
 export default App;
