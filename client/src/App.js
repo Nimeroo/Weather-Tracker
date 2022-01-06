35f46034fc070f20bc547df3559d3f2eab41d7b9
Nimeroo <girou.joe@gmail.com>
2022-01-06 06:00:41 -0500

    submitting a location into the input receives a successful response from the API

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,38 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { getWeatherForecast } from "./Services/api-config";
+import { locationURLParse } from "./Util/locationUrlParse";
 
 function App() {
   const [selectedWeather, setSelectedWeather] = useState({});
   const [forecast, setForecast] = useState([]);
   const [location, setLocation] = useState("");
 
-  useEffect(async () => {
+  const currentForecast = async () => {
     let fetchForecast = await getWeatherForecast(location);
     setForecast(fetchForecast);
-  }, [location]);
+  };
+
+  console.log(forecast);
 
   return (
     <div className="App">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          currentForecast();
+        }}
+      >
+        <label>
+          <input
+            onChange={(e) => {
+              setLocation(locationURLParse(e.target.value));
+            }}
+            placeholder="Enter your location"
+          ></input>
+        </label>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
