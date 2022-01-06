24f353efb35a3af6e053b019ab0f9c17555ff508
Nimeroo <girou.joe@gmail.com>
2022-01-06 05:38:34 -0500

    locations automatically gain a + between words for url parameter

--- client/src/Util/locationUrlParse.js
+++ client/src/Util/locationUrlParse.js
@@ -1,7 +1,5 @@
-const locationURLParse = (location) => {
+export const locationURLParse = (location) => {
     let splitLocation = location.split(" ");
     let rejoinedLocation = splitLocation.join("+")
     return rejoinedLocation;
   }
-
-export default locationURLParse;
