de1a3eb12d72ffaa7122d7d9e9a857c370e433dc
Nimeroo <girou.joe@gmail.com>
2022-01-09 17:06:37 -0500

    Loading conditional added to main

--- client/src/Screens/Main/Main.jsx
+++ client/src/Screens/Main/Main.jsx
@@ -2,10 +2,23 @@ import SelectedWeather from "../../Components/SelectedWeather/SelectedWeather";
 import ForecastList from "../../Components/ForecastList/ForecastList";
 import LocationInput from "../../Components/LocationInput/LocationInput";
 
-const Main = ({forecast, selectedDay, handleIndex, handleSubmit, handleLocation}) => {
+const Main = ({
+  forecast,
+  selectedDay,
+  handleIndex,
+  handleSubmit,
+  handleLocation,
+}) => {
+  if (!forecast) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
     <div>
-      <LocationInput handleSubmit={handleSubmit} handleLocation={handleLocation}/>  
+      <LocationInput
+        handleSubmit={handleSubmit}
+        handleLocation={handleLocation}
+      />
       <SelectedWeather weatherInfo={forecast} selectedDay={selectedDay} />
       <ForecastList weatherInfo={forecast} setSelectedDay={handleIndex} />
     </div>
