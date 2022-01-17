8bbb65c0e2ca785aae6fd4745b5a90e6cd6ce5d1
Nimeroo <girou.joe@gmail.com>
2022-01-17 06:51:36 -0500

    container divs replaced with paper components from MUI

--- client/src/Components/ForecastList/ForecastList.jsx
+++ client/src/Components/ForecastList/ForecastList.jsx
@@ -1,5 +1,6 @@
-import "./ForecastList.css"
+import "./ForecastList.css";
 import react from "react";
+import { Paper } from "@mui/material";
 import { dateFormatter } from "../../Util/dateFormatter";
 
 const ForecastList = ({ weatherInfo, setSelectedDay }) => {
@@ -7,27 +8,21 @@ const ForecastList = ({ weatherInfo, setSelectedDay }) => {
     //Indices are hard coded in due to there only needing to be 3 items in the list and the first item must be treated differently
 
     <div id="forecast-list">
-      <div
-        onClick={() => setSelectedDay(0)}
-        src={weatherInfo.current.condition.icon}
-      >
+      <Paper elevation={10} onClick={() => setSelectedDay(0)}>
         <h3>{dateFormatter(`${weatherInfo.forecast.forecastday[0].date}`)}</h3>
-        <h4>{`${weatherInfo.current.temp_f}°`}</h4>
-      </div>
-      <div
-        onClick={() => setSelectedDay(1)}
-        src={weatherInfo.forecast.forecastday[1].day.condition.icon}
-      >
+        <img src={weatherInfo.current.condition.icon}></img>
+        <h4>{`${weatherInfo.current.temp_f} F°`}</h4>
+      </Paper>
+      <Paper elevation={10} onClick={() => setSelectedDay(1)}>
         <h3>{dateFormatter(`${weatherInfo.forecast.forecastday[1].date}`)}</h3>
-        <h4>{`${weatherInfo.forecast.forecastday[1].day.avgtemp_f}°`}</h4>
-      </div>
-      <div
-        onClick={() => setSelectedDay(2)}
-        src={weatherInfo.forecast.forecastday[2].day.condition.icon}
-      >
+        <img src={weatherInfo.forecast.forecastday[1].day.condition.icon}></img>
+        <h4>{`${weatherInfo.forecast.forecastday[1].day.avgtemp_f} F°`}</h4>
+      </Paper>
+      <Paper elevation={10} onClick={() => setSelectedDay(2)}>
         <h3>{dateFormatter(`${weatherInfo.forecast.forecastday[2].date}`)}</h3>
-        <h4>{`${weatherInfo.forecast.forecastday[2].day.avgtemp_f}°`}</h4>
-      </div>
+        <img src={weatherInfo.forecast.forecastday[2].day.condition.icon}></img>
+        <h4>{`${weatherInfo.forecast.forecastday[2].day.avgtemp_f} F°`}</h4>
+      </Paper>
     </div>
   );
 };
