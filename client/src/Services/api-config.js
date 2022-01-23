cff437942ab61785a9006506d191a06a88f1744a
Nimeroo <girou.joe@gmail.com>
2022-01-23 06:32:15 -0500

    changes for deployment

--- client/src/Services/api-config.js
+++ client/src/Services/api-config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const domain = "http://api.weatherapi.com/v1";
+const domain = "https://api.weatherapi.com/v1";
 const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
 
 export const getWeatherForecast = async (location) => {
