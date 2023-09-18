b628f751ab8db05af14b95020d48aac5234e9169
Nimeroo <girou.joe@gmail.com>
2023-09-18 09:42:43 -0400

    Autocomplete component added to main screen

--- client/src/Screens/Main/Main.jsx
+++ client/src/Screens/Main/Main.jsx
@@ -3,6 +3,8 @@ import SelectedWeather from "../../Components/SelectedWeather/SelectedWeather";
 import ForecastList from "../../Components/ForecastList/ForecastList";
 import LocationInput from "../../Components/LocationInput/LocationInput";
 import WeatherDetails from "../../Components/WeatherDetails/WeatherDetails";
+import CityAutocomplete from "../../Components/CityAutocomplete/CityAutocomplete";
+
 
 const Main = ({
   scrollTo,
@@ -11,6 +13,10 @@ const Main = ({
   selectedDay,
   handleIndex,
   handleLocation,
+  setInput,
+  input,
+  setSubmit,
+  submit
 }) => {
   if (!forecast) {
     return <h1>Loading...</h1>;
@@ -21,11 +27,21 @@ const Main = ({
       <LocationInput
         forecast={forecast}
         handleLocation={handleLocation}
+        setInput={setInput}
+        input={input}
+        setSubmit={setSubmit}
+        submit={submit}
       />
-      {forecast.status === 200 && <><WeatherDetails weatherInfo={forecast} selectedDay={selectedDay}>
-        <SelectedWeather weatherInfo={forecast} selectedDay={selectedDay} />
-      </WeatherDetails>
-        <ForecastList weatherInfo={forecast} setSelectedDay={handleIndex} scrollTo={scrollTo} mainReference={mainReference} /></>}
+      {
+        !submit && input.length > 1 && <CityAutocomplete setInput={setInput} input={input} setSubmit={setSubmit} handleLocation={handleLocation}/>
+      }
+      {
+        forecast.status === 200 &&
+        <><WeatherDetails weatherInfo={forecast} selectedDay={selectedDay}>
+          <SelectedWeather weatherInfo={forecast} selectedDay={selectedDay} />
+        </WeatherDetails>
+          <ForecastList weatherInfo={forecast} setSelectedDay={handleIndex} scrollTo={scrollTo} mainReference={mainReference} /></>
+      }
     </div>
   );
 };
