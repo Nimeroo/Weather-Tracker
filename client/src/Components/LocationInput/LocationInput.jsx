f7917ff94be623c8c8e4666bf05ebc4941d90b3b
Nimeroo <girou.joe@gmail.com>
2023-09-18 09:40:40 -0400

    useState expanded in scope and moved out of form

--- client/src/Components/LocationInput/LocationInput.jsx
+++ client/src/Components/LocationInput/LocationInput.jsx
@@ -1,14 +1,12 @@
 import "./LocationInput.css";
-import { useEffect, useState } from "react";
 import { TextField, IconButton } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import { locationURLParse } from "../../Util/locationUrlParse.js";
 import { useLocation } from "react-router-dom";
 
-const LocationInput = ({ handleLocation, forecast }) => {
+
+const LocationInput = ({ setInput, input, setSubmit, submit, handleLocation, forecast }) => {
   const routeLocation = useLocation();
-  const [input, setInput] = useState("");
-  const [submit, setSubmit] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -19,15 +17,19 @@ const LocationInput = ({ handleLocation, forecast }) => {
     <div id="form-cont">
       <form
         onSubmit={(e) => {
-          handleLocation(input);
           handleSubmit(e);
+          handleLocation(locationURLParse(input));
         }}
       >
         <TextField
           fullWidth
+          autoComplete="off"
           onChange={(e) => {
-            setInput(locationURLParse(e.target.value))
-          }}
+            setSubmit(false)
+            setInput(e.target.value)
+          }
+          }
+          onFocus={() => setSubmit(false)}
           error={
             (submit && forecast.status === 400) || (submit && input === "" && forecast.status == 400)
           }
@@ -46,6 +48,7 @@ const LocationInput = ({ handleLocation, forecast }) => {
           </IconButton>
         ) : null}
       </form>
+
     </div>
   );
 };
